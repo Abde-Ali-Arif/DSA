cf339821db2f06c2b4b69e1cd9fc4b2512d03fba
Abde Ali Arif <abdealiarif@gmail.com>
2026-04-14 11:47:21 +0530

    Time: 3 ms (89.68%) | Memory: 44.9 MB (90.99%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,15 +1,57 @@
 class Solution {
+    // Brute force O(n^2)
+    // public int lengthOfLongestSubstring(String s) {
+    //     int maxLen=0;
+
+    //     for(int i=0 ;i<s.length();i++){
+    //         int[] hash=new int[256];
+    //         for(int j =i;j<s.length();j++){
+    //             if(hash[s.charAt(j)] == 1)break;
+    //             hash[s.charAt(j)] = 1;
+    //             int len=j-i+1;
+    //             maxLen=Math.max(maxLen,len);
+    //         }
+    //     }
+    //     return maxLen;
+    // }
+
+    // Better But r is not moving by one step everytime
+    // public int lengthOfLongestSubstring(String s) {
+    //     int maxLen=0;
+    //     int l=0 ,r=0;
+
+    //     int[] hash=new int[256];
+    //     while(r<s.length()){
+    //         if(hash[s.charAt(r)] == 1){
+    //             hash[s.charAt(l)]=0;
+    //             l++;
+    //         }else{
+    //             hash[s.charAt(r)]=1;
+    //             maxLen=Math.max(maxLen,r-l+1);
+    //             r++;
+    //         }
+    //     }
+    //     return maxLen;
+    // }
+    
+
+    // optimal O(n)
     public int lengthOfLongestSubstring(String s) {
         int maxLen=0;
+        int l=0 ,r=0;
 
-        for(int i=0 ;i<s.length();i++){
-            int[] hash=new int[256];
-            for(int j =i;j<s.length();j++){
-                if(hash[s.charAt(j)] == 1)break;
-                hash[s.charAt(j)] = 1;
-                int len=j-i+1;
-                maxLen=Math.max(maxLen,len);
+        int[] hash=new int[256];
+        // fill index as -1
+        Arrays.fill(hash,-1);
+
+        while(r < s.length()){
+            if(hash[s.charAt(r)] != -1 && hash[s.charAt(r)] >= l){
+                // the above condition tells that the char is already seen and is in range
+                l = hash[s.charAt(r)]+1;
             }
+            maxLen=Math.max(maxLen,r-l+1);
+            hash[s.charAt(r)]=r;
+            r++;
         }
         return maxLen;
     }
