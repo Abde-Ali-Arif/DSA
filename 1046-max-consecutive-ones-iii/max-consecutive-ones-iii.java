0c324b12e314f2ae574ed8a5cbea7c55c6102e08
Abde Ali Arif <abdealiarif@gmail.com>
2026-04-14 12:51:39 +0530

    Time: 3 ms (97.43%) | Memory: 52.4 MB (9.54%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -1,22 +1,35 @@
 class Solution {
-    public int longestOnes(int[] nums, int k) {
-        int l=0,r=0,maxLen=0;
+    // public int longestOnes(int[] nums, int k) {
+    //     int l=0,r=0,maxLen=0;
+    //     while(r < nums.length){
+    //         if(nums[r] == 1){
+    //             r++;
+    //         }
+    //         else if (nums[r] == 0 && k > 0){
+    //             k--;
+    //             r++;
+    //         }else{
+    //             if(nums[l] == 1)l++;
+    //             else{
+    //                 l++;
+    //                 k++;
+    //             }
+    //         }
+    //         maxLen=Math.max(maxLen,r-l);
+    //     }
+    //     return maxLen;
+    // }
 
+    public int longestOnes(int[] nums, int k) {
+        int l=0,r=0,maxLen=0,zeroes=0;
         while(r < nums.length){
-            if(nums[r] == 1){
-                r++;
+            if(nums[r] == 0)zeroes++;
+            while(zeroes > k){
+                if(nums[l] == 0)zeroes--;
+                l++;
             }
-            else if (nums[r] == 0 && k > 0){
-                k--;
-                r++;
-            }else{
-                if(nums[l] == 1)l++;
-                else{
-                    l++;
-                    k++;
-                }
-            }
-            maxLen=Math.max(maxLen,r-l);
+            maxLen=Math.max(maxLen,r-l+1);
+            r++;
         }
         return maxLen;
     }
