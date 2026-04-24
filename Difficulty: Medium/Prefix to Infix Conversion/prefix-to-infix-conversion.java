e3f1acd206da52a09d0696fc8ef861959f38cb3a
Abde Ali Arif <abdealiarif@gmail.com>
2026-04-24 08:38:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Prefix to Infix Conversion/prefix-to-infix-conversion.java
+++ Difficulty: Medium/Prefix to Infix Conversion/prefix-to-infix-conversion.java
@@ -2,17 +2,9 @@
 
 class Solution {
     
-    public static int precedence(char ch){
-        if(ch == '^')return 3;
-        else if(ch == '*' || ch == '/')return 2;
-        else if(ch == '+' || ch == '-')return 1;
-        return 0;
-    }
-    
     static String preToInfix(String pre_exp) {
         int n = pre_exp.length();
         Stack<String> st= new Stack<>();
-        StringBuilder ans = new StringBuilder();
 
         for(int i=n-1;i>=0;i--){
             char ch = pre_exp.charAt(i);
@@ -20,15 +12,10 @@ class Solution {
                 st.push(ch+"");
             }
             else if(!Character.isLetterOrDigit(ch)){
-                    ans.append("(" + st.pop() + ch + st.pop() + ")");
-                    // ans.append(st.pop());
-                    // ans.append(ch);
-                    // ans.append(st.pop()+")");
-                    st.push(ans.toString());
-                    ans.setLength(0);
+                    String ans = "(" + st.pop() + ch + st.pop() + ")";
+                    st.push(ans);
             }
         }
-        ans.append(st.pop());
-        return ans.toString();
+        return st.pop();
     }
 }
