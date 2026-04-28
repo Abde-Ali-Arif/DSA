a05c37c9f22acc98a46aeb09f5cc2dfb4b1d8944
Abde Ali Arif <abdealiarif@gmail.com>
2026-04-28 11:37:14 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (15.21%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -14,38 +14,38 @@
  * }
  */
 class Solution {
-    // public List<Integer> inorderTraversal(TreeNode root) {
-    //     List<Integer> res = new ArrayList<>();
-    //     if(root == null)return res;
-
-    //     res.addAll(inorderTraversal(root.left));
-    //     res.add(root.val);
-    //     res.addAll(inorderTraversal(root.right));
-    //     return res;
-    // }
-
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         if(root == null)return res;
 
-        Stack<TreeNode> st= new Stack<>();
-        TreeNode curr = root;
-
-        while(curr != null || !st.isEmpty()) {
-
-            // Go to leftmost node
-            while(curr != null) {
-                st.push(curr);
-                curr = curr.left;
-            }
-
-            // Process node
-            curr = st.pop();
-            res.add(curr.val);
-
-            // Move to right subtree
-            curr = curr.right;
-        }
+        res.addAll(inorderTraversal(root.left));
+        res.add(root.val);
+        res.addAll(inorderTraversal(root.right));
         return res;
     }
+
+    // public List<Integer> inorderTraversal(TreeNode root) {
+    //     List<Integer> res = new ArrayList<>();
+    //     if(root == null)return res;
+
+    //     Stack<TreeNode> st= new Stack<>();
+    //     TreeNode curr = root;
+
+    //     while(curr != null || !st.isEmpty()) {
+
+    //         // Go to leftmost node
+    //         while(curr != null) {
+    //             st.push(curr);
+    //             curr = curr.left;
+    //         }
+
+    //         // Process node
+    //         curr = st.pop();
+    //         res.add(curr.val);
+
+    //         // Move to right subtree
+    //         curr = curr.right;
+    //     }
+    //     return res;
+    // }
 }
