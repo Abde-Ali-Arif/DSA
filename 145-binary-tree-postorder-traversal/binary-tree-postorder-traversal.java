778468eef048e37905d1e71458e2b02772c3f0f3
Abde Ali Arif <abdealiarif@gmail.com>
2026-04-28 20:18:51 +0530

    Time: 1 ms (11.82%) | Memory: 43.1 MB (70.24%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -14,13 +14,54 @@
  * }
  */
 class Solution {
+    
+    // //  Recursive Solution
+    // public List<Integer> postorderTraversal(TreeNode root) {
+    //     List<Integer> res = new ArrayList<>();
+    //     if(root == null)return res;
+
+    //     res.addAll(postorderTraversal(root.left));
+    //     res.addAll(postorderTraversal(root.right));
+    //     res.add(root.val);
+    //     return res;
+    // }
+
+    // // Iterative solution using Two Stacks
+    // public List<Integer> postorderTraversal(TreeNode root) {
+    //     List<Integer> res = new ArrayList<>();
+    //     if(root == null)return res;
+
+    //     Stack<TreeNode> st= new Stack<>();
+    //     Stack<Integer> ans =new Stack<>();
+    //     st.push(root);
+    //     while(!st.isEmpty()){
+    //         TreeNode temp = st.pop();
+    //         ans.push(temp.val);
+    //         if(temp.left != null) st.push(temp.left);
+    //         if(temp.right != null) st.push(temp.right);
+    //     }
+    //     while(!ans.isEmpty()){
+    //         res.add(ans.pop());
+    //     }
+    //     return res;
+    // }
+
+
+    // Iterative solution using single Stack
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         if(root == null)return res;
 
-        res.addAll(postorderTraversal(root.left));
-        res.addAll(postorderTraversal(root.right));
-        res.add(root.val);
+        Stack<TreeNode> st= new Stack<>();
+        st.push(root);
+        while(!st.isEmpty()){
+            TreeNode temp = st.pop();
+            res.add(temp.val);
+            if(temp.left != null) st.push(temp.left);
+            if(temp.right != null) st.push(temp.right);
+        }
+        Collections.reverse(res);
         return res;
     }
+
 }
