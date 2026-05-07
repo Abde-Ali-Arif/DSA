de6a3e2207b884ca1ab34eb23470cd49bcad50db
Abde Ali Arif <abdealiarif@gmail.com>
2026-05-07 23:22:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.java
@@ -0,0 +1,22 @@
+class Solution {
+    public ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
+        ArrayList<Integer> bfs = new ArrayList<>();
+        boolean[] vis= new boolean[adj.size()];
+        
+        Queue<Integer> q = new LinkedList<>();
+        q.add(0);
+        vis[0]=true;
+        
+        while(!q.isEmpty()){
+            Integer node = q.poll();
+            bfs.add(node);
+            for(Integer it: adj.get(node)){
+                if(!vis[it]) {
+                    vis[it]=true;
+                    q.add(it);
+                }
+            }
+        }
+        return bfs;
+    }
+}
