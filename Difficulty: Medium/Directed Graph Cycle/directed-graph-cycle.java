35b50a8b83be16c63f4831777afafdd9860a7085
Abde Ali Arif <abdealiarif@gmail.com>
2026-05-11 00:00:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
@@ -17,6 +17,36 @@ class Solution {
         vis[node]=1;
         return false;
     }
+    
+    public boolean bfs( ArrayList<ArrayList<Integer>> adj,int[] vis){
+        int V=adj.size();
+        Queue<Integer> q = new LinkedList<>();
+        int[] indegree = new int[V];
+        int cnt=0;  // this will cnt numbers in toposort
+        for(int i=0;i<V;i++){
+            for(int it:adj.get(i)){
+                indegree[it]++;
+            }
+        }
+        
+        for(int i=0;i<V;i++){
+            if(indegree[i] == 0)q.add(i);
+        }
+        
+        while(!q.isEmpty()){
+            int node = q.poll();
+            cnt++;
+            // reduce indegree of its neighbours 
+            for(int it:adj.get(node)){
+                indegree[it]--;
+                if(indegree[it] == 0)q.add(it);
+            }
+        }
+        
+        return cnt != V;
+        
+    }
+    
     public boolean isCyclic(int V, int[][] edges) {
         if(V == 0)return false;
         int[] vis = new int[V];
@@ -30,12 +60,14 @@ class Solution {
         }
         
         // Traversal for DFS
-        for(int i=0;i<V;i++){
-            if(vis[i] == 0){
-                // if any of the component returns true then return it no need to check further
-                if(dfs(i,adj,vis))return true;
-            }
-        }
+        // for(int i=0;i<V;i++){
+        //     if(vis[i] == 0){
+        //         // if any of the component returns true then return it no need to check further
+        //         if(dfs(i,adj,vis))return true;
+        //     }
+        // }
+        
+        if(bfs(adj,vis))return true;
         return false;
     }
 }
