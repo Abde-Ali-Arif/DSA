5416d8ae51c17acee4b78494b047b95afae2fa6e
Abde Ali Arif <abdealiarif@gmail.com>
2026-05-14 19:16:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Pyramid Pattern/pyramid-pattern.java
@@ -0,0 +1,15 @@
+class Solution {
+    void printPyramid(int n) {
+        StringBuilder ans= new StringBuilder();
+        for(int i=1;i <= n;i++){
+            for(int j=1;j<= n-i ;j++){
+                ans.append(" ");
+            }
+            for(int j=1;j<=2*i-1;j++){
+                ans.append("*");
+            }
+            ans.append("\n");
+        }
+        System.out.print(ans.toString());
+    }
+}
