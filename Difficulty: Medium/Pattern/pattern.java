52694bbda3e1174d4c26c7fa2f9cc0f336f50cfd
Abde Ali Arif <abdealiarif@gmail.com>
2026-05-22 16:18:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Pattern/pattern.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void printDiamond(int n) {
+        for(int i=1;i<=2*n;i++){
+            if(i<=n){
+                for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+                }
+                for(int j=1;j<=i;j++){
+                System.out.print("* ");
+                }
+                System.out.println();
+            }else{
+                for(int j=1;j<=n-(2*n-i+1);j++){
+                System.out.print(" ");
+                }
+                for(int j=1;j<=2*n-i+1;j++){
+                System.out.print("* ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
