60838fc647e22858254252a0585689fa6d5b1a9f
Abde Ali Arif <abdealiarif@gmail.com>
2026-05-25 15:37:47 +0530

    Time: 15 ms (30.91%) | Memory: 56.1 MB (25.76%) - LeetSync

---
+++ 2001-jump-game-vii/jump-game-vii.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean canReach(String s, int minJump, int maxJump) {
+
+        int n = s.length();
+
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(0);
+
+        boolean[] vis = new boolean[n];
+        vis[0] = true;
+
+        int farthest = 0;
+
+        while (!q.isEmpty()) {
+
+            int idx = q.poll();
+
+            if (idx == n - 1) return true;
+
+            int start = Math.max(idx + minJump, farthest + 1);
+            int end = Math.min(idx + maxJump, n - 1);
+
+            for (int j = start; j <= end; j++) {
+
+                if (s.charAt(j) == '0' && !vis[j]) {
+                    vis[j] = true;
+                    q.offer(j);
+                }
+            }
+
+            farthest = end;
+        }
+
+        return false;
+    }
+}
