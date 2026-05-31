877a9c65c83a7e58db33ee9d5e034747e7c2de62
Abde Ali Arif <abdealiarif@gmail.com>
2026-05-31 13:10:31 +0530

    Time: 6 ms (10.45%) | Memory: 50.3 MB (54.22%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -15,6 +15,28 @@ class Solution {
     //     return grid[row][col] + min;
     // }
 
+    // Memoization
+    // public int minPathSum(int[][] grid) {
+    //     int m=grid.length;
+    //     int n=grid[0].length;
+    //     int[][] path=new int[m][n];
+    //     for(int[] it:path){
+    //         Arrays.fill(it,Integer.MAX_VALUE);
+    //     }
+    //     return func(path,m-1,n-1,m,n,grid);
+    // }
+    // public int func(int[][] path,int row,int col,int m,int n,int[][] grid){
+    //     if(row < 0 || row >= m || col < 0 || col >= n ) return Integer.MAX_VALUE;
+    //     if(row == 0 && col == 0)return grid[row][col];
+    //     if(path[row][col] != Integer.MAX_VALUE)return path[row][col];
+    //     int up=func(path,row,col-1,m,n,grid);
+    //     int left=func(path,row-1,col,m,n,grid);
+    //     int min =Math.min(up,left);
+    //     if(min == Integer.MAX_VALUE)min=0;
+    //     return path[row][col] = grid[row][col] +min;
+    // }
+
+    // Tabulation
     public int minPathSum(int[][] grid) {
         int m=grid.length;
         int n=grid[0].length;
@@ -22,16 +44,19 @@ class Solution {
         for(int[] it:path){
             Arrays.fill(it,Integer.MAX_VALUE);
         }
-        return func(path,m-1,n-1,m,n,grid);
-    }
-    public int func(int[][] path,int row,int col,int m,int n,int[][] grid){
-        if(row < 0 || row >= m || col < 0 || col >= n ) return Integer.MAX_VALUE;
-        if(row == 0 && col == 0)return grid[row][col];
-        if(path[row][col] != Integer.MAX_VALUE)return path[row][col];
-        int up=func(path,row,col-1,m,n,grid);
-        int left=func(path,row-1,col,m,n,grid);
-        int min =Math.min(up,left);
-        if(min == Integer.MAX_VALUE)min=0;
-        return path[row][col] = grid[row][col] +min;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i == 0 && j == 0) path[0][0]=grid[0][0];
+                else{
+                    int up=Integer.MAX_VALUE;
+                    int left=Integer.MAX_VALUE;
+                    if(i > 0) up=path[i-1][j];
+                    if(j > 0) left=path[i][j-1];
+                    int min =Math.min(up,left);
+                    path[i][j] = grid[i][j] + min;
+                }
+            }
+        }
+        return path[m-1][n-1];
     }
 }
