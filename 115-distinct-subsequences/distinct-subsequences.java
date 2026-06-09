1a084bd4fc2ef1d6af6599c4c324cb347cf6a542
Abde Ali Arif <abdealiarif@gmail.com>
2026-06-09 17:22:41 +0530

    Time: 16 ms (83.70%) | Memory: 42.6 MB (97.84%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -48,6 +48,26 @@ class Solution {
     // }
 
     // Space optimization O(n*m)
+    // public int numDistinct(String s, String t) {
+    //     int n=s.length();
+    //     int m = t.length();
+    //     int[] prev = new int[m+1];
+    //     // Base case if t is over then one possible answer found
+    //     prev[0]=1;
+
+    //     for(int i=1;i<=n;i++){
+    //         int[] curr = new int[m+1];
+    //         curr[0]=1;
+    //         for(int j=1;j<=m;j++){
+    //             if(s.charAt(i-1) == t.charAt(j-1)) curr[j]=prev[j-1]+ prev[j];
+    //             else curr[j] = prev[j];
+    //         }
+    //         prev = curr;
+    //     }
+    //     return prev[m];
+    // }
+
+    // Space optimization using single array O(n*m)
     public int numDistinct(String s, String t) {
         int n=s.length();
         int m = t.length();
@@ -56,13 +76,9 @@ class Solution {
         prev[0]=1;
 
         for(int i=1;i<=n;i++){
-            int[] curr = new int[m+1];
-            curr[0]=1;
-            for(int j=1;j<=m;j++){
-                if(s.charAt(i-1) == t.charAt(j-1)) curr[j]=prev[j-1]+ prev[j];
-                else curr[j] = prev[j];
+            for(int j=m;j>=1;j--){
+                if(s.charAt(i-1) == t.charAt(j-1)) prev[j]=prev[j-1]+ prev[j];
             }
-            prev = curr;
         }
         return prev[m];
     }
