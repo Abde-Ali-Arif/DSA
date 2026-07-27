2e62fae0cc784534217b3cba11758e179728b72d
Abde Ali Arif <abdealiarif@gmail.com>
2026-07-27 17:33:30 +0530

    Time: 3 ms (86.49%) | Memory: 47.2 MB (93.88%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.java
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.java
@@ -1,11 +1,43 @@
 class Solution {
+    // public int maximumProduct(int[] nums) {
+    //     int n= nums.length;
+    //     Arrays.sort(nums);
+
+    //     int option1 = nums[n - 1] * nums[n - 2] * nums[n - 3];
+        
+    //     int option2 = nums[0] * nums[1] * nums[n - 1];
+        
+    //     return Math.max(option1, option2);
+    // }
+
     public int maximumProduct(int[] nums) {
         int n= nums.length;
-        Arrays.sort(nums);
 
-        int option1 = nums[n - 1] * nums[n - 2] * nums[n - 3];
+        int m1=-1000,m2=-1000,m3=-1000;
+        int min1=1000,min2=1000;
+
+        for(int i=0;i<n;i++){
+            if(nums[i] > m1){
+                m3=m2;
+                m2=m1;
+                m1=nums[i];
+            }else if(nums[i] > m2){
+                m3=m2;
+                m2=nums[i];
+            }else if(nums[i] > m3){
+                m3=nums[i];
+            }
+            if(nums[i] < min1){
+                min2=min1;
+                min1=nums[i];
+            }else if(nums[i] < min2){
+                min2 = nums[i];
+            }
+        }
+
+        int option1 = m1*m2*m3;
         
-        int option2 = nums[0] * nums[1] * nums[n - 1];
+        int option2 = min1* min2 * m1;
         
         return Math.max(option1, option2);
     }
