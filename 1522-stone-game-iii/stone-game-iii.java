84b9e4b22f9295e045a20e1f11548f71eda715b3
Abde Ali Arif <abdealiarif@gmail.com>
2026-08-03 23:08:10 +0530

    Time: 11 ms (65.34%) | Memory: 85.5 MB (71.59%) - LeetSync

---
+++ 1522-stone-game-iii/stone-game-iii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String stoneGameIII(int[] stoneValue) {
+
+        int n = stoneValue.length;
+
+        int[] dp = new int[n + 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            dp[i] = Integer.MIN_VALUE;
+
+            int sum = 0;
+
+            for (int take = 1; take <= 3 && i + take - 1 < n; take++) {
+
+                sum += stoneValue[i + take - 1];
+
+                dp[i] = Math.max(dp[i], sum - dp[i + take]);
+            }
+        }
+
+        if (dp[0] > 0)
+            return "Alice";
+        else if (dp[0] < 0)
+            return "Bob";
+
+        return "Tie";
+    }
+}
