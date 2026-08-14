2ea72b44dfb6722946e75ce286e87a80bd1cb745
Abde Ali Arif <abdealiarif@gmail.com>
2026-08-14 17:44:17 +0530

    Time: 1 ms (100.00%) | Memory: 43.3 MB (94.88%) - LeetSync

--- 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.java
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.java
@@ -1,16 +1,38 @@
 class Solution {
+
+    // O(n^2)
+    // public int maximumLengthSubstring(String s) {
+    //     int n=s.length();
+    //     int len=0;
+    //     for(int i=0;i<n;i++){
+    //         int[] hash=new int[26];
+    //         for(int j=i;j<n;j++){
+    //             int ind=s.charAt(j)-'a';
+    //             hash[ind]++;
+    //             if(hash[ind] > 2) break;
+    //             len=Math.max(len,j-i+1);
+    //         }
+    //     }
+    //     return len;
+    // }
+
+    // O(2n)
     public int maximumLengthSubstring(String s) {
         int n=s.length();
-        int len=0;
-        for(int i=0;i<n;i++){
-            int[] hash=new int[26];
-            boolean flag=false;
-            for(int j=i;j<n;j++){
-                int ind=s.charAt(j)-'a';
-                hash[ind]++;
-                if(hash[ind] > 2) break;
-                len=Math.max(len,j-i+1);
+        int[] hash=new int[26];
+        int len=0,l=0,r=0;
+
+        while(r < n){
+            int ind=s.charAt(r)-'a';
+            hash[ind]++;
+            
+            while(hash[ind] > 2){
+                int nind = s.charAt(l)-'a';
+                hash[nind]--;
+                l++;
             }
+            len=Math.max(len,r-l+1);
+            r++;
         }
         return len;
     }
